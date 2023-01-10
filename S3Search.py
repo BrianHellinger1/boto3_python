f0fdb1a69349221b61058a9c362aa7bf8a17b604
BrianHellinger1 <bphellinger@gmail.com>
2023-01-10 01:58:59 +0000

    This is an another script to search for all s3 buckets

---
+++ S3Search.py
@@ -0,0 +1,15 @@
+# list All S3 Buckets
+
+import boto3
+
+s3 = boto3.client('s3')
+
+
+response = s3.list_buckets()
+
+
+buckets = response["Buckets"]
+
+
+for bucket in buckets:
+    print(bucket["Name"])
